34b004f2cc0e9b0b88cee4e52e7c4507da7f2834
Prateek Aghi <prateekaghi42@gmail.com>
2022-12-22 12:03:49 +0530

    .

--- Expenses/src/components/NewExpenseComp/ExpenseForm.js
+++ Expenses/src/components/NewExpenseComp/ExpenseForm.js
@@ -1,87 +1,44 @@
-import { useState } from "react";
+import { useRef } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [entetedAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+  const enteredTitle = useRef();
+  const enteredAmount = useRef();
+  const enteredDate = useRef();
 
   const cancelForm = () => {
     props.onCancel();
   };
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
-  const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
-  };
-
-  const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput((prevState) => {
-      return { ...prevState, enteredAmount: event.target.value };
-    });
-  };
-
-  const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput((prevState) => {
-      return { ...prevState, enteredDate: event.target.value };
-    });
-  };
-
   const submitHandler = (event) => {
     event.preventDefault();
     const expenseData = {
-      title: userInput.enteredTitle,
-      amount: +userInput.enteredAmount,
-      date: new Date(userInput.enteredDate),
+      title: enteredTitle.current.value,
+      amount: +enteredAmount.current.value,
+      date: new Date(enteredDate.current.value),
     };
 
     props.onSaveExpenseData(expenseData);
-    setUserInput((prevState) => {
-      return {
-        ...prevState,
-        enteredAmount: "",
-        enteredDate: "",
-        enteredTitle: "",
-      };
-    });
   };
   return (
     <form onSubmit={submitHandler}>
+      {enteredTitle.current}
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input
-            type="text"
-            value={userInput.enteredTitle}
-            onChange={titleChangeHandler}
-          />
+          <input type="text" ref={enteredTitle} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            value={userInput.enteredAmount}
-            onChange={amountChangeHandler}
-          />
+          <input type="number" min="0.01" ref={enteredAmount} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
           <input
             type="date"
-            value={userInput.enteredDate}
             min="2019-01-01"
             max="2022-12-31"
-            onChange={dateChangeHandler}
+            ref={enteredDate}
           />
         </div>
       </div>
